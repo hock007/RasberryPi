5ae3e9256abbdbc68cbe81e9eb641a308b2299bb
ManiSharma786 <hockwili007@gmail.com>
2017-09-18 22:37:58 +0530

    click listener on Button

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+# schema for Rashberry pi
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
+Window.size = (1000, 840)
 
 Builder.load_file("main.kv")
 
@@ -10,9 +14,23 @@ class MainScreen(Screen):
     pass
 
 
+class LoginScreen(Screen):
+    pass
+
+
+class NextScreen(Screen):
+    pass
+
+
+rashberry = ScreenManager()
+rashberry.add_widget(MainScreen(name='menu'))
+rashberry.add_widget(LoginScreen(name='login'))
+rashberry.add_widget(NextScreen(name='next'))
+
+
 class MyApp(App):
     def build(self):
-        return MainScreen()
+        return rashberry
 
 if __name__ == '__main__':
     MyApp().run()
